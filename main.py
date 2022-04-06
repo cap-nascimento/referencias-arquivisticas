c85e7ead15e9d8b94293d8f1fe82b5fcadf02d62
cap-nascimento <anascimentovieira2@gmail.com>
2022-04-06 17:14:25 -0300

    adicionando mais coisas

--- main.py
+++ main.py
@@ -29,19 +29,25 @@ renamed_df = df.rename(columns=change_labels, inplace=False)
 # print(len(renamed_df))
 is_tese = renamed_df['tese-dissertacao'] == 'T'
 is_dissertacao = renamed_df['tese-dissertacao'] == 'D'
-nor_tese_nor_dis = renamed_df['tese-dissertacao'] == None
+count = 0
 
-teses_df = renamed_df[is_tese]
-temp = renamed_df.iloc[[122]]
-print(temp['tese-dissertacao'])
-dissertacoes_df = renamed_df[is_dissertacao]
-no_category = renamed_df[nor_tese_nor_dis]
+for index, line in renamed_df.iterrows():
+	if line['tese-dissertacao'] == None:
+		count+=1
+# print(count)
 
-print(len(teses_df))
-print(len(dissertacoes_df))
-print(len(no_category))
-print(len(teses_df) + len(dissertacoes_df))
+# print(temp['tese-dissertacao'].to_string())
+
+# dissertacoes_df = renamed_df[is_dissertacao]
+# no_category = renamed_df[nor_tese_nor_dis]
 
 # no category: 124, 140, 1444, 1882, 2161
 
-df.to_csv("referencias_arquivisticas.csv", index=False)
+teses = renamed_df[is_tese]
+dissertacoes = renamed_df[is_dissertacao]
+
+print(teses)
+print(dissertacoes)
+
+teses.to_csv("filtro_teses.csv", index=False)
+dissertacoes.to_csv("filtro_dissercoes.csv", index=False)
