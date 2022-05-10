044de61cba63155113e4e977ff1315456bb10c57
RafaelJBOne <61058172+RafaelJBOne@users.noreply.github.com>
2022-05-09 23:02:14 -0300

    Teste de links referências arquivisticas
    Os seguintes arquivos são respectivamente, código que executa testes nos links, tabela com os links pesquisados e resultado do processamento.
    Aparentemente os resultados não estão como esperado, deve-se avaliar se realmente há algum erro nas pesquisas e registro dos resultados. 
    Obs: código elaborado Johnatan.

---
+++ links.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests
+
+
+requests.packages.urllib3.disable_warnings()
+
+# Abre o arquivo de links usando a biblioteca pandas
+dfs = pd.read_excel(r"Links.xlsx", sheet_name=None)
+
+resultado = open("resultado.csv", "a+")
+
+# Cria o cabeçalho do arquivo csv de resultado
+resultado.write("TESE / DISSERTAÇÃO;TITULO DA REFERÊNCIA;LINK;STATUS LINK\n")
+
+count = 0
+
+# Loop nas tabelas do arquivo de links
+for name, sheet in dfs.items():
+
+    # Loop nas linhas das tabelas do arquivo de links
+    for index, linha in sheet.iterrows():
+        
+        print(count, end="\r")
+
+        # Verificação do link
+        status = "OK"
+        try:
+            r = requests.get(linha["LINK"], verify=False)
+
+            # O Status 200 significa que a pagina foi retornada sem erros
+            if r.status_code != 200:
+                status = "Erro"
+        except Exception as e:
+            print(e)
+            status = "Erro"
+
+        # Guardando a linha no arquivo csv de resultado
+        resultado.write("{};{};{};{}\n".format(linha["TESE / DISSERTAÇÃO"], linha["TÍTULO DA REFERÊNCIA"], linha["LINK"], status))
+        count = count + 1
